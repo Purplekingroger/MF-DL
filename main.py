45e1f71614114eae9b61d9a4b515f75a61c84788
Purplekingroger <brendanplp01@gmail.com>
2025-05-01 22:40:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from api.mediafire import MediaFireAPI
 if __name__ == "__main__":
     mf = MediaFireAPI(sleep_per_download=2)
 
-    url = ""
+    url = "https://www.mediafire.com/?6yk4v28x8uck5#6yk4v28x8uck5"
     folder_key = url.split("folder/")[-1].split("/")[0]
 
     mf.download_folder(folder_key, "output")
